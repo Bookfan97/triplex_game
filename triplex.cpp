3aa08f78a7a863bc60d386c860ff3af588d53cc4
Noah <sheltielover432@gmail.com>
2020-03-08 23:18:23 -0400

    Up to Quiz 1

--- triplex.cpp
+++ triplex.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 int main()
 {
+    //print welcome messages to the terminal
     std::cout <<"You are a secret agent breaking into a secure server room";
     std::cout << std::endl;
     std::cout <<"You need to enter the correct codes to enter...";
+
+    //declare variables
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    //print values to the terminal
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
