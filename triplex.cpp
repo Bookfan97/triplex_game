1a719c170eb7205c6f126b168b71e4e0be913e4f
Noah <sheltielover432@gmail.com>
2020-03-08 23:06:53 -0400

    Hello World

---
+++ triplex.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    std::cout <<"You are a secret agent breaking into a secure server room";
+    std::cout << std::endl;
+    std::cout <<"You need to enter the correct codes to enter...";
+    return 0;
+}
