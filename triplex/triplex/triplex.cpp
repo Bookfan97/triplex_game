16b1318b07407c05cf2ee9d1b4143ca411167b7f
Noah <sheltielover432@gmail.com>
2020-03-09 22:32:48 -0400

    Quiz 2

--- triplex/triplex/triplex.cpp
+++ triplex/triplex/triplex.cpp
@@ -1,23 +1,62 @@
 #include <iostream>
+
+void PrintIntroduction()
+{
+    std::cout << "-------------------------------------------------------------\n";
+    std::cout << "| You are a secret agent breaking into a secure server room |\n";
+    std::cout << "| You need to enter the correct codes to enter...           |\n";
+    std::cout << "-------------------------------------------------------------\n";
+}
+
+bool PlayGame()
+{
+    PrintIntroduction();
+
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+
+    // Print CodeSum and CodeProduct to the terminal
+    std::cout << "| There are 3 numbers in the code                           | \n";
+    std::cout << "| The codes add-up to: " << CodeSum <<"                                    | \n";
+    std::cout << "| The codes multiply to give: " << CodeProduct << "                            |" << std::endl;
+    std::cout << "-------------------------------------------------------------\n";
+    std::cout << "Enter the codes: " << std::endl;
+    // Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    // Check if the players guess is correct
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "!!!!!!!!!!!!\n";
+        std::cout << "| You win! |\n";
+        std::cout << "!!!!!!!!!!!!\n";
+        return true;
+    }
+    else
+    {
+        std::cout << "=============\n";
+        std::cout << "! You Lose! |\n";
+        std::cout << "=============\n";
+        return false;
+    }
+}
+
 int main()
 {
-    //print welcome messages to the terminal
-    std::cout << "You are a secret agent breaking into a secure server room";
-    std::cout << std::endl;
-    std::cout << "You need to enter the correct codes to enter...";
-
-    //declare variables
-    int a = 4;
-    int b = 3;
-    int c = 2;
-
-    int sum = a + b + c;
-    int product = a * b * c;
-
-    //print values to the terminal
-    std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    while (true)
+    {
+        bool bLevelComplete = PlayGame();
+        std::cin.clear(); // Clears any errors
+        std::cin.ignore(); // Discards the buffer
+    }
 
     return 0;
 }
